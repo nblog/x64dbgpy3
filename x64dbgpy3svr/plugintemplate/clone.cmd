76fa2c6d38b0d55b0371d2f32e98e7f7a2a83217
r0th3r <503407184@qq.com>
2023-04-04 17:44:56 +0800

    update

--- x64dbgpy3svr/plugintemplate/clone.cmd
+++ x64dbgpy3svr/plugintemplate/clone.cmd
@@ -19,5 +19,5 @@ for /l %%i in (0, 1, 1) do ( curl -L %GIT%blob/main/src/!PLUGIN[%%i]!.h?raw=true
 
 :: pluginsdk
 curl %GHPROXY%https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2023-03-04_02-26.zip -o x64dbg.zip
-tar -zxvf x64dbg.zip pluginsdk/
+tar -xf x64dbg.zip pluginsdk/
 del /Q x64dbg.zip
