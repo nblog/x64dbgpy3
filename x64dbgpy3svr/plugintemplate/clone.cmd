b82071d8ed4b377ac0893a62dfe462627284941c
nblog <503407184@qq.com>
2025-04-16 17:22:10 +0800

    update

--- x64dbgpy3svr/plugintemplate/clone.cmd
+++ x64dbgpy3svr/plugintemplate/clone.cmd
@@ -16,6 +16,7 @@ for /l %%i in (0, 1, 1) do ( curl -L %X64TEMPLATE%blob/main/src/!PLUGIN[%%i]!.h?
 
 :: pluginsdk
 :: https://api.github.com/repos/x64dbg/x64dbg/releases/latest
-curl -L https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2025-03-15_15-57.zip -o x64dbg.zip
+set SNAPSHOT=snapshot_2023-06-10_18-05
+curl -L https://github.com/x64dbg/x64dbg/releases/download/snapshot/%SNAPSHOT%.zip -o x64dbg.zip
 tar -xf x64dbg.zip pluginsdk/
 del /Q x64dbg.zip
